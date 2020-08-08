01c851b29b6538196b329408b268de9dd5187354
Edyesid <edyesid2000@gmail.com>
2020-08-08 00:47:56 -0500

    segundo avance

--- src/main/java/edu/escuelaing/arep/app/App.java
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -1,13 +1,38 @@
 package edu.escuelaing.arep.app;
 
-/**
- * Hello world!
- *
- */
+import java.io.*;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+    	App app = new App();
+    	app.Reader("table1/column1.txt");
+    }
+    
+    public void Reader (String path) {
+    	File file = null;
+    	FileReader fileReader = null;
+    	BufferedReader bufferedReader = null;
+    	
+    	try {
+    		file = new File(path);
+    		fileReader = new FileReader(file);
+    		bufferedReader = new BufferedReader(fileReader);
+    		
+    		String line;
+    		while ((line = bufferedReader.readLine()) != null) {
+    			System.out.println(line);
+    		}
+    	} catch(Exception e) {
+    		e.printStackTrace();
+    	} finally {
+    		try {
+    			if(null != fileReader) {
+    				fileReader.close();
+    			}
+    		} catch (Exception e2) {
+    			e2.printStackTrace();
+    		}
+    	}
     }
 }
